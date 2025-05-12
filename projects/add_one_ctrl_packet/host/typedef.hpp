a42c59c614bab52a91ee4bc5e400ace2b9fcae72
joeldushouyu <joeldushouyu@gmail.com>
2025-05-12 19:13:02 -0400

    working add_one_ctro_packet

---
+++ projects/add_one_ctrl_packet/host/typedef.hpp
@@ -0,0 +1,54 @@
+#ifndef __TYPEDEF_H__
+#define __TYPEDEF_H__
+#include <bits/stdc++.h>
+#include <boost/program_options.hpp>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <stdfloat>
+
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+#include "buffer.hpp"
+
+
+
+
+typedef int8_t dtype_in;
+typedef int32_t dtype_out;
+
+
+typedef int16_t DATATYPE_IN1;
+typedef int32_t DATATYPE_IN2;
+typedef int16_t DATATYPE_OUT;
+#endif
+// #ifndef __TYPEDEF_H__
+// #define __TYPEDEF_H__
+// #include <bits/stdc++.h>
+// #include <boost/program_options.hpp>
+// #include <chrono>
+// #include <cstdint>
+// #include <cstdlib>
+// #include <ctime>
+// #include <fstream>
+// #include <iomanip>
+// #include <iostream>
+// #include <sstream>
+// #include <stdfloat>
+
+// #include "xrt/xrt_bo.h"
+// #include "xrt/xrt_device.h"
+// #include "xrt/xrt_kernel.h"
+// #include "vector_view.hpp"
+
+// typedef int8_t dtype_in;
+// typedef int32_t dtype_out;
+
+
+// #endif
